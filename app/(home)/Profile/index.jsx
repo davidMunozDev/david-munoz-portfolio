d103fbc40af044c6b30ed2d837258f4f19b26e4f
davidMunozDev <davidmunozdev@gmail.com>
2024-01-31 12:59:44 +0100

    create profile section

--- app/(home)/Profile/index.jsx
+++ app/(home)/Profile/index.jsx
@@ -1,10 +1,32 @@
 import styles from "./profile.module.scss";
+import Image from "next/image";
+import Button from "@/app/components/Button";
+import Icon from "@/app/components/Icon";
+//TODO add images, icons with component and button
 
 export default function Profile() {
   return (
     <div className={styles.Wrapper}>
-      <div className={styles.Name}></div>
-      <div className={styles.Content}></div>
+      <div className={styles.ProfileInformation}>
+        <Image
+          alt="profile image"
+          src="/img/me-avatar.jpeg"
+          width="82"
+          height="82"
+          className={styles.Avatar}
+        />
+        <div className={styles.Information}>
+          <h2 className={styles.Name}>David Muñoz</h2>
+          <p className={styles.Description}>
+            Always looking for new opportunities and focus on improve.
+          </p>
+        </div>
+      </div>
+      <div className={styles.Social}>
+        <Icon name="linkedIn" />
+        <Icon name="gitHub" />
+        <Button>Download CV</Button>
+      </div>
     </div>
   );
 }
