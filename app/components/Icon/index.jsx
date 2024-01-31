c85cb0ee0af2fd84c5bbd8f4c5a407ec912eed7c
davidMunozDev <davidmunozdev@gmail.com>
2024-01-31 13:00:55 +0100

    create icon and button components

---
+++ app/components/Icon/index.jsx
@@ -0,0 +1,22 @@
+import Image from "next/image";
+
+const getIcon = (name) => {
+  const icons = {
+    gitHub: "github-dark",
+    linkedIn: "linkedIn",
+    calendar: "calendar",
+  };
+
+  return `/icons/${icons[name]}.svg`;
+};
+
+export default function Icon({ name, width = "28", height = "28" }) {
+  return (
+    <Image
+      alt={`icon ${name}`}
+      width={width}
+      height={height}
+      src={getIcon(name)}
+    />
+  );
+}
